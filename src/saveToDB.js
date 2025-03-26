abb76c05f800a888602191bbede8cdb419b105a9
Brendan Price <56489848+dev-brendanprice@users.noreply.github.com>
2025-03-26 21:51:13 +0000

    Add check for dev or prod

--- src/saveToDB.js
+++ src/saveToDB.js
@@ -2,11 +2,20 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const mode = process.env.MODE;
 const pool = mysql.createPool({
-    host: `${process.env.DEV_DB_HOST}`,
-    user: `${process.env.DEV_DB_USER}`,
-    password: `${process.env.DEV_DB_PASSWORD}`,
-    database: `${process.env.DEV_DB_NAME}`,
+    host: `${mode === 'production' ? process.env.DB_HOST : process.env.DEV_DB_HOST}`,
+    user: `${mode === 'production' ? process.env.DB_USER : process.env.DEV_DB_USER}`,
+    password: `${mode === 'production' ? process.env.DB_PASSWORD : process.env.DEV_DB_PASSWORD}`,
+    database: `${mode === 'production' ? process.env.DB_NAME : process.env.DEV_DB_NAME}`,
+    connectionLimit: 25,
+    waitForConnections: true
+});
+console.log({
+    host: `${mode === 'production' ? process.env.DB_HOST : process.env.DEV_DB_HOST}`,
+    user: `${mode === 'production' ? process.env.DB_USER : process.env.DEV_DB_USER}`,
+    password: `${mode === 'production' ? process.env.DB_PASSWORD : process.env.DEV_DB_PASSWORD}`,
+    database: `${mode === 'production' ? process.env.DB_NAME : process.env.DEV_DB_NAME}`,
     connectionLimit: 25,
     waitForConnections: true
 });
